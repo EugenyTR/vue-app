00cca7b4404b8663452dd30ea152a83d50da2016
EugenyTR <55189944+EugenyTR@users.noreply.github.com>
2020-01-14 22:16:05 +0700

    npm i router and adding router-link

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,13 @@
 <template>
   <div id="app">
     <h1>To do application</h1>
-    <AddTodo
-      @add-todo="addTodo"
-    />
     <hr>
-    <TodoList
-      v-bind:todos="todos"
-      @remove-todo="removeTodo"
-    />
+
+    <router-view />
   </div>
 </template>
 
-<script>
-import TodoList from '@/components/TodoList'
-import AddTodo from '@/components/AddTodo'
-export default {
-  name: 'app',
-  data() {
-    return {
-      todos: [
-        {id: 1, title: 'Купить хлеб', completed: false},
-        {id: 2, title: 'Купить масло', completed: false},
-        {id: 3, title: 'Купить пиво', completed: false},
-      ]
-    }
-  },
-  methods: {
-    removeTodo(id) {
-      this.todos = this.todos.filter(t => t.id !== id)
-    },
-    addTodo(todo) {
-      this.todos.push(todo)
-    }
-  },
-  components: {
-    TodoList, AddTodo
-  }
-}
-</script>
+
 
 <style>
 #app {
