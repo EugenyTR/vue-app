a01b5db85d8212a5ff0a973f4a471f7e512cb0cf
EugenyTR <55189944+EugenyTR@users.noreply.github.com>
2020-01-14 22:56:37 +0700

    add object computed and var to get filter array

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -4,7 +4,7 @@
             <input type="checkbox"
                    v-on:change="todo.completed = !todo.completed">
             <strong>{{index + 1}}</strong>
-            {{todo.title}}
+            {{todo.title | uppercase}}
         </span>
         <button class="rm" v-on:click="$emit('remove-todo', todo.id)"
         >&times;</button>
@@ -19,6 +19,12 @@ export default {
             required: true
         },
         index: Number
+    },
+    filters: {
+        uppercase(value) {
+            //console.log(value)
+           return value.toUpperCase()
+        }
     }
 }
 </script>
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -6,30 +6,62 @@
         <AddTodo
                 @add-todo="addTodo"
         />
+        <select v-model="filter">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="not-completed">Not completed</option>
+        </select>
         <hr>
+        <Loader v-if="loading"/>
         <TodoList
-                v-bind:todos="todos"
+                v-else-if="filteredTodos.length"
+                v-bind:todos="filteredTodos"
                 @remove-todo="removeTodo"
         />
+        <p v-else>No todos!</p>
     </div>
 </template>
 
 <script>
     import TodoList from '@/components/TodoList'
     import AddTodo from '@/components/AddTodo'
+    import Loader from '@/components/Loader'
     export default {
         name: 'app',
         data() {
             return {
-                todos: []
+                todos: [],
+                loading: true,
+                filter: 'all'
             }
         },
         mounted() {
             fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
                 .then(response => response.json())
                 .then(json => {
-                    this.todos = json
+                    setTimeout(() => {
+                        this.todos = json
+                        this.loading = false
+                    }, 1000)
                 })
+        },
+        //watch: {
+        //    filter(value) {
+        //        console.log(value)
+        //    }
+        //},
+        computed: {
+            filteredTodos() {
+                if (this.filter === 'all') {
+                    return this.todos
+                }
+                if (this.filter === 'completed') {
+                    return this.todos.filter(t => t.completed)
+                }
+                if (this.filter === 'not-completed') {
+                    return this.todos.filter(t => !t.completed)
+                }
+            }
         },
         methods: {
             removeTodo(id) {
@@ -40,7 +72,7 @@
             }
         },
         components: {
-            TodoList, AddTodo
+            TodoList, AddTodo, Loader
         }
     }
 </script>
